390141ea6cee8d42ac7c556cea2b46c517e9fe93
jackorn ( JIGAR SONI ) <51534139+jigarsoni17@users.noreply.github.com>
2023-02-06 23:33:43 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,6 +2,10 @@ variable "region" {
   default = "us-east-1"
 }
 
+variable "profile" {
+    description = "AWS credentials profile you want to use"
+}
+
 variable "number_of_public_subnets" {
   default = 2
 }
